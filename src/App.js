5b21793c73112560d38c91a7183ceccb0ad5b755
Kirill Maksimov <kirillm@realconnex.com>
2019-03-12 11:52:38 +0300

    question with react

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
-import Header from '../src/Header';
-import MainContent from '../src/MainContent';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      click: 0
+    }
+    this.addOneClick = this.addOneClick.bind(this);
+    this.removeOneClick = this.removeOneClick.bind(this);
+  }
+
+  addOneClick() {
+    this.setState(state => state.click++ );
+  }
+
+  removeOneClick() {
+    this.setState(state => state.click--);
+  }
+
   render() {
     return (
-      <div>
-        <Header />
-        {/* <MainContent /> */}
-        {/*<Footer />*/}
-      </div> 
+      <section>
+        <div style = {{ backgroundColor: 'green' }}>
+          Click me {this.state.click}
+        </div> 
+        <button onClick = { this.addOneClick }>Add one</button>
+        <button onClick = { this.removeOneClick }>Remove one</button>
+      </section>
     );
   }
 }
